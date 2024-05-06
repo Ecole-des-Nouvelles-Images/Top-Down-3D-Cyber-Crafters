dd5c7a88af8e820ac6d23196bef5f5cc933d2e6f
ElkTargets <meneroudvincent@gmail.com>
2024-05-06 14:20:37 +0200

    Player Controller and Inputs

---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        [Header("ID du Spawn")] public int spawnId; // ID du spawn utile à l'assignation du joueur
+    }
+}
