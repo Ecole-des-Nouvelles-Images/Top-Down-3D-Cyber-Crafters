f7c4dcd7a4d7c68e4f2f7d1a7247f298803ca4dd
sky-ark <a.renaux@nouvellesimages.xyz>
2024-05-10 15:16:41 +0200

    trappe + ventilo

--- Assets/Scripts/Traps/VentiloTrap/VentiloTrap.cs
+++ Assets/Scripts/Traps/VentiloTrap/VentiloTrap.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using Enemies;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class VentiloTrap : Trap
 {
