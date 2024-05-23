5aa3eb77381db228678971b841405a834111db51
ElkTargets <meneroudvincent@gmail.com>
2024-05-23 16:31:00 +0200

    Starting Gare

--- Assets/Scripts/Gare/Station.cs
+++ Assets/Scripts/Gare/Station.cs
@@ -1,18 +1,59 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Station : MonoBehaviour
+namespace Gare
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Station : MonoBehaviour
     {
-        
-    }
+        public GameObject wagonShopGameObject;
+        private bool rotating;
+        private Quaternion targetRotation;
+        private float rotationDuration = 1f;
+        private float rotationTimeElapsed = 0f;
+        public int WagonIndex;
+        public List<GameObject> shopWagons = new List<GameObject>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void FixedUpdate()
+        {
+            
+            if (rotating)
+            {
+                rotationTimeElapsed += Time.deltaTime;
+                float rotationProgress = rotationTimeElapsed / rotationDuration;
+                wagonShopGameObject.transform.rotation = Quaternion.Lerp(wagonShopGameObject.transform.rotation, targetRotation, rotationProgress);
+
+                if (rotationProgress >= 1f)
+                {
+                    rotating = false;
+                    rotationTimeElapsed = 0f;
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    shopWagons[WagonIndex].SetActive(false);
+                    WagonIndex -= 1;
+                    if (WagonIndex > 5) { WagonIndex = 0; }
+                    if (WagonIndex < 0) { WagonIndex = 5; }
+                    shopWagons[WagonIndex].SetActive(true);
+                    rotating = true;
+                    targetRotation = Quaternion.Euler(wagonShopGameObject.transform.eulerAngles.x, wagonShopGameObject.transform.eulerAngles.y, wagonShopGameObject.transform.eulerAngles.z + 60f);
+                }
+
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    shopWagons[WagonIndex].SetActive(false);
+                    WagonIndex += 1;
+                    if (WagonIndex > 5) { WagonIndex = 0; }
+                    if (WagonIndex < 0) { WagonIndex = 5; }
+                    shopWagons[WagonIndex].SetActive(true);
+                    rotating = true;
+                    targetRotation = Quaternion.Euler(wagonShopGameObject.transform.eulerAngles.x, wagonShopGameObject.transform.eulerAngles.y, wagonShopGameObject.transform.eulerAngles.z - 60f);
+                }
+            }
+        }
     }
 }
+
