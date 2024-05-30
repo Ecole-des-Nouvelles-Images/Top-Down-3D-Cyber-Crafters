624c37789cc0a66fe1b4351e58e4bdd9f4208c14
sky-ark <a.renaux@nouvellesimages.xyz>
2024-05-30 12:21:19 +0200

    trapplaydetector a finir

--- Assets/Proto/Script/Trap/TrapPlayerDetector.cs
+++ Assets/Proto/Script/Trap/TrapPlayerDetector.cs
@@ -5,16 +5,23 @@ using UnityEngine;
     public class TrapPlayerDetector : MonoBehaviour
     {
         public GameObject uiGameObject;
+        public GameObject leaveTrapUI;
 
         private void Awake() {
             uiGameObject.SetActive(false);
         }
 
         private void OnTriggerEnter(Collider other) {
-            if (other.CompareTag("Player")) { uiGameObject.SetActive(true); }
+            if (other.CompareTag("Player"))
+            {
+                uiGameObject.SetActive(true);
+            }
         }
         private void OnTriggerExit(Collider other) {
-            if (other.CompareTag("Player")) { uiGameObject.SetActive(false); }
+            if (other.CompareTag("Player"))
+            {
+                uiGameObject.SetActive(false);
+            }
         }
         
     }
